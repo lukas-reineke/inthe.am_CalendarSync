9d1379fe42880b5208b3c756dea0c552990d74a6
Lukas Reineke <lukas.reineke@protonmail.com>
2020-02-24 22:31:22 +0900

    store credentials in local folder

--- intheAMsync.py
+++ intheAMsync.py
@@ -31,12 +31,10 @@ def get_credentials():
     Returns:
         Credentials, the obtained credential.
     """
-    home_dir = os.path.expanduser('~')
-    credential_dir = os.path.join(home_dir, '.credentials')
-    if not os.path.exists(credential_dir):
-        os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir,
-                                   'calendar-python-quickstart.json')
+    credential_path = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        '.credentials.json',
+    )
 
     store = Storage(credential_path)
     credentials = store.get()
